f5f55f72c32393aa30d19b663fc5d70773a217af
Jelte De Proft <jelte.deproft@intix.eu>
2020-03-30 11:53:55 +0200

    small changes

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -4,18 +4,16 @@ import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.badlogic.gdx.tools.texturepacker.TexturePacker;
-import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
 import com.mygdx.game.Norii;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-	    config.title = "Norii a turn based strategy game by Jelte & Julian";
-	    config.width = 840;
-	    config.height = 840;
-	    
-		Application app = new LwjglApplication(new Norii(), config);
+	public static void main(final String[] arg) {
+		final LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Norii a turn based strategy game by Jelte & Julian";
+		config.width = 640;
+		config.height = 640;
+
+		final Application app = new LwjglApplication(new Norii(), config);
 		Gdx.app = app;
 		Gdx.app.setLogLevel(Application.LOG_DEBUG);
 	}
