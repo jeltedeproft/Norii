d52909e88c7400787012efa81686a029597bea5a
Jelte De Proft <jelte.deproft@intix.eu>
2021-08-13 14:15:02 +0200

    improved unit test battlestate grid helper

--- tests/src/testBattleState/TestBattleStateGridHelperFromUnits.java
+++ tests/src/testBattleState/TestBattleStateGridHelperFromUnits.java
@@ -68,8 +68,8 @@ public class TestBattleStateGridHelperFromUnits {
 		ability.getSpellData().setAreaOfEffectRange(2);
 		ability.getSpellData().setLineOfSight("LINE");
 		final Array<MyPoint> unitPositions = new Array<>();
-		unitPositions.addAll(new MyPoint[] { testPoints[7][7], testPoints[6][6], testPoints[4][4], testPoints[3][3], testPoints[3][4], testPoints[4][3], testPoints[7][6], testPoints[6][7], testPoints[6][4], testPoints[7][4], testPoints[6][3], testPoints[7][3], testPoints[4][6],
-				testPoints[4][7], testPoints[3][6], testPoints[3][7] });
+		unitPositions.addAll(new MyPoint[] { testPoints[7][7], testPoints[6][6], testPoints[4][4], testPoints[3][3], testPoints[3][4], testPoints[4][3], testPoints[7][6], testPoints[6][7], testPoints[6][4], testPoints[7][4],
+				testPoints[6][3], testPoints[7][3], testPoints[4][6], testPoints[4][7], testPoints[3][6], testPoints[3][7] });
 		final Array<MyPoint> results = gridHelper.getTargetPositionsInRangeAbility(center, ability, allPoints);
 		unitPositions.sort();
 		results.sort();
@@ -84,8 +84,8 @@ public class TestBattleStateGridHelperFromUnits {
 		ability.getSpellData().setAreaOfEffectRange(2);
 		ability.getSpellData().setLineOfSight("LINE");
 		final Array<MyPoint> unitPositions = new Array<>();
-		unitPositions.addAll(new MyPoint[] { testPoints[5][6], testPoints[5][7], testPoints[5][4], testPoints[5][3], testPoints[4][5], testPoints[3][5], testPoints[6][5], testPoints[7][5], testPoints[5][5], testPoints[8][5], testPoints[9][5], testPoints[2][5], testPoints[1][5],
-				testPoints[5][8], testPoints[5][9], testPoints[5][2], testPoints[5][1] });
+		unitPositions.addAll(new MyPoint[] { testPoints[5][6], testPoints[5][7], testPoints[5][4], testPoints[5][3], testPoints[4][5], testPoints[3][5], testPoints[6][5], testPoints[7][5], testPoints[5][5], testPoints[8][5],
+				testPoints[9][5], testPoints[2][5], testPoints[1][5], testPoints[5][8], testPoints[5][9], testPoints[5][2], testPoints[5][1] });
 		final Array<MyPoint> results = gridHelper.getTargetPositionsInRangeAbility(center, ability, allPoints);
 		unitPositions.sort();
 		results.sort();
@@ -100,9 +100,10 @@ public class TestBattleStateGridHelperFromUnits {
 		ability.getSpellData().setAreaOfEffectRange(2);
 		ability.getSpellData().setLineOfSight("LINE");
 		final Array<MyPoint> unitPositions = new Array<>();
-		unitPositions.addAll(new MyPoint[] { testPoints[5][5], testPoints[5][6], testPoints[5][7], testPoints[5][8], testPoints[5][9], testPoints[5][4], testPoints[5][3], testPoints[5][2], testPoints[5][1], testPoints[6][5], testPoints[6][6], testPoints[6][7], testPoints[6][8],
-				testPoints[6][4], testPoints[6][3], testPoints[6][2], testPoints[7][5], testPoints[7][6], testPoints[7][7], testPoints[7][4], testPoints[7][3], testPoints[8][5], testPoints[8][6], testPoints[8][4], testPoints[9][5], testPoints[4][5], testPoints[4][6],
-				testPoints[4][7], testPoints[4][8], testPoints[4][4], testPoints[4][3], testPoints[4][2], testPoints[3][5], testPoints[3][6], testPoints[3][7], testPoints[3][4], testPoints[3][3], testPoints[2][5], testPoints[2][6], testPoints[2][4], testPoints[1][5] });
+		unitPositions.addAll(new MyPoint[] { testPoints[5][5], testPoints[5][6], testPoints[5][7], testPoints[5][8], testPoints[5][9], testPoints[5][4], testPoints[5][3], testPoints[5][2], testPoints[5][1], testPoints[6][5],
+				testPoints[6][6], testPoints[6][7], testPoints[6][8], testPoints[6][4], testPoints[6][3], testPoints[6][2], testPoints[7][5], testPoints[7][6], testPoints[7][7], testPoints[7][4], testPoints[7][3], testPoints[8][5],
+				testPoints[8][6], testPoints[8][4], testPoints[9][5], testPoints[4][5], testPoints[4][6], testPoints[4][7], testPoints[4][8], testPoints[4][4], testPoints[4][3], testPoints[4][2], testPoints[3][5], testPoints[3][6],
+				testPoints[3][7], testPoints[3][4], testPoints[3][3], testPoints[2][5], testPoints[2][6], testPoints[2][4], testPoints[1][5] });
 		final Array<MyPoint> results = gridHelper.getTargetPositionsInRangeAbility(center, ability, allPoints);
 		unitPositions.sort();
 		results.sort();
@@ -165,5 +166,45 @@ public class TestBattleStateGridHelperFromUnits {
 		target = new MyPoint(0, 5);
 		results = gridHelper.getAllCastPointsWhereTargetIsHit(ability, target, center, battleState);
 		assertEquals(unitPositions, results);
+	}
+
+	@Test
+	public void testCastPointsTargetHitLineSquareRange2AoeRange2() {
+		final BattleState battleState = new BattleState(20, 20);
+		final Ability ability = new Ability(AbilitiesEnum.FIREBALL);
+		ability.getSpellData().setAreaOfEffect("SQUARE");
+		ability.getSpellData().setRange(4);
+		ability.getSpellData().setAreaOfEffectRange(2);
+		ability.getSpellData().setLineOfSight("LINE");
+
+		Set<MyPoint> expectedReturnValues = new HashSet<>(Arrays.asList(testPoints[5][6], testPoints[5][7], testPoints[5][8], testPoints[5][9], testPoints[3][5], testPoints[4][5], testPoints[6][5], testPoints[7][5]));
+		MyPoint targetUnit = new MyPoint(5, 7);
+		Set<MyPoint> results = gridHelper.getAllCastPointsWhereTargetIsHit(ability, targetUnit, center, battleState);
+		assertEquals(expectedReturnValues, results);
+
+		expectedReturnValues = new HashSet<>(Arrays.asList(testPoints[5][4], testPoints[5][3], testPoints[5][2], testPoints[5][1], testPoints[3][5], testPoints[4][5], testPoints[6][5], testPoints[7][5]));
+		targetUnit = new MyPoint(5, 3);
+		results = gridHelper.getAllCastPointsWhereTargetIsHit(ability, targetUnit, center, battleState);
+		assertEquals(expectedReturnValues, results);
+
+		expectedReturnValues = new HashSet<>();
+		targetUnit = new MyPoint(5, 12);
+		results = gridHelper.getAllCastPointsWhereTargetIsHit(ability, targetUnit, center, battleState);
+		assertEquals(expectedReturnValues, results);
+
+		expectedReturnValues = new HashSet<>(Arrays.asList(testPoints[5][7], testPoints[5][4], testPoints[3][5], testPoints[5][6], testPoints[4][5], testPoints[5][3], testPoints[2][5], testPoints[1][5]));
+		targetUnit = new MyPoint(3, 5);
+		results = gridHelper.getAllCastPointsWhereTargetIsHit(ability, targetUnit, center, battleState);
+		assertEquals(expectedReturnValues, results);
+
+		expectedReturnValues = new HashSet<>(Arrays.asList(testPoints[5][6], testPoints[5][7], testPoints[5][8], testPoints[5][9], testPoints[6][5], testPoints[7][5], testPoints[4][5], testPoints[8][5]));
+		targetUnit = new MyPoint(6, 7);
+		results = gridHelper.getAllCastPointsWhereTargetIsHit(ability, targetUnit, center, battleState);
+		assertEquals(expectedReturnValues, results);
+
+		expectedReturnValues = new HashSet<>(Arrays.asList(testPoints[2][5], testPoints[1][5]));
+		targetUnit = new MyPoint(0, 5);
+		results = gridHelper.getAllCastPointsWhereTargetIsHit(ability, targetUnit, center, battleState);
+		assertEquals(expectedReturnValues, results);
 	}
 }
