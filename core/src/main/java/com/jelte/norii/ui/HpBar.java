e69bdc26ab641af4bc15a371346cc208431bd2e6
Jelte <jeltedeproft@hotmail.com>
2021-08-28 15:39:21 +0200

    fix hp-bars

--- core/src/main/java/com/jelte/norii/ui/HpBar.java
+++ core/src/main/java/com/jelte/norii/ui/HpBar.java
@@ -10,6 +10,7 @@ public class HpBar {
 
 	private static final int OFFSET_BAR_Y = 12;
 	private static final float MIN_VALUE = 0f;
+	private static final float MAX_VALUE = 100f;
 	private static final float STEP_SIZE = 1f;
 	private static final float ANIMATION_DURATION = 0.25f;
 	public static final int HEALTHBAR_Y_OFFSET = 50;
@@ -32,17 +33,17 @@ public class HpBar {
 			progressBarStyle = statusUISkin.get("red-hp-bar", ProgressBarStyle.class);
 		}
 
-		healthBar = new ProgressBar(MIN_VALUE, entity.getEntityData().getMaxHP(), STEP_SIZE, false, progressBarStyle);
-		healthBar.setValue(entity.getEntityData().getMaxHP());
+		healthBar = new ProgressBar(MIN_VALUE, MAX_VALUE, STEP_SIZE, false, progressBarStyle);
+		healthBar.setValue(MAX_VALUE);
 		healthBar.setAnimateDuration(ANIMATION_DURATION);
 		healthBar.setPosition(entity.getVisualComponent().getEntityactor().getX() * tilePixelWidth, ((entity.getVisualComponent().getEntityactor().getY() * tilePixelHeight) + OFFSET_BAR_Y));
-		healthBar.setWidth(tilePixelWidth);
+		healthBar.setWidth((tilePixelWidth / 3) * 2);
 
 		progressBarStyle.background.setMinHeight(tilePixelHeight / HEIGHT_FACTOR);
 		progressBarStyle.knobBefore.setMinHeight(tilePixelHeight / HEIGHT_FACTOR);
 
-		progressBarStyle.background.setMinWidth(tilePixelWidth);
-		progressBarStyle.knobBefore.setMinWidth(tilePixelWidth);
+		progressBarStyle.background.setMinWidth((tilePixelWidth / 3) * 2);
+		// progressBarStyle.knobBefore.setMinWidth(tilePixelWidth);
 	}
 
 	public ProgressBar getHealthBar() {
@@ -51,7 +52,7 @@ public class HpBar {
 
 	public void update(Entity unit) {
 		getHealthBar().setPosition(unit.getVisualComponent().getEntityactor().getX() * tilePixelWidth, ((unit.getVisualComponent().getEntityactor().getY() * tilePixelHeight) + HEALTHBAR_Y_OFFSET));
-		getHealthBar().setValue(unit.getHp());
+		getHealthBar().setValue(((float) unit.getHp() / (float) unit.getEntityData().getMaxHP()) * 100.0f);
 		if (getHealthBar().getValue() == 0) {
 			getHealthBar().setVisible(false);
 		}
